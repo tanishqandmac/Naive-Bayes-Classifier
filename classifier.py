d5d268672d2c81578bad8c09ff986ff6521b74ed
tanishqandmac <tanishq15105@iiitd.ac.in>
2018-09-19 18:44:37 +0530

    v1.1

--- classifier.py
+++ classifier.py
@@ -15,80 +15,100 @@ def dictAddition(dictionary,string):
                 dictionary[token] += 1.0
     return dictionary
 
+def datasetDictCreation(filename):
+    SCount = 0.0
+    NCount = 0.0
+    f = open(filename)
+    for sentence in f:
+        sentenceToken = sentence.split('\t')
+        try:
+            sentenceDictionary.append({'text':sentenceToken[2].encode('utf8'),'category':sentenceToken[1]})
+            if(sentenceToken[1]=="0"):
+                SCount+=1.0
+            else:
+                NCount+=1.0
+        except:
+            continue
+    return sentenceDictionary,SCount,NCount
+
+def separateDictionaryCreation(notSarcasmDict,SarcasmDict,sentenceDictionary):
+    for k in sentenceDictionary:
+        if k['category']=="0":
+            notSarcasmDict = dictAddition(notSarcasmDict,k['text'])
+        else:
+            SarcasmDict = dictAddition(SarcasmDict,k['text'])
+    return SarcasmDict,notSarcasmDict
+
+def pickleLoad(filename):
+    with open(filename, "rb") as f:
+        filetype = pickle.load(f)
+    return filetype
+
+def pickleUnload(filename,filetype):
+    with open(filename, "wb") as f:
+        pickle.dump(filetype, f)
+
+def bayesianClassification(testSentence,SarcasmDict,notSarcasmDict,SCount,NCount):
+    bayesProbability_1 = 1
+    bayesProbability_0 = 1
+    tokens = nltk.word_tokenize(testSentence.lower())
+    tokenFiltered = []
+    for token in tokens:
+        if token not in stop_words:
+            tokenFiltered.append(token)
+
+    for token in tokenFiltered:
+        try:
+            bayesProbability_1 *= (1.0+SarcasmDict[token])/(len(SarcasmDict)+len(DictionaryAll))
+            bayesProbability_0 *= (1.0+notSarcasmDict[token])/(len(notSarcasmDict)+len(DictionaryAll))
+        except:
+            bayesProbability_1 *= (1.0)/(len(SarcasmDict)+len(DictionaryAll))
+            bayesProbability_0 *= (1.0)/(len(notSarcasmDict)+len(DictionaryAll))
+    
+    bayesProbability_1 *= (SCount/SCount+NCount)
+    bayesProbability_0 *= (NCount/SCount+NCount)
+
+    return bayesProbability_1,bayesProbability_0
+
+#Variable Declaration
 sentenceDictionary = []
 SarcasmDict={}
 notSarcasmDict={}
 DictionaryAll = {}
-bayesProbability_1 = 1
-bayesProbability_0 = 1
 SCount = 0.0
 NCount = 0.0
+filename = 'trainedDataset.tsv'
 
-f = open('trainedDataset.tsv')
-for sentence in f:
-    sentenceToken = sentence.split('\t')
-    try:
-        sentenceDictionary.append({'text':sentenceToken[2].encode('utf8'),'category':sentenceToken[1]})
-        if(sentenceToken[1]=="0"):
-            SCount+=1.0
-        else:
-            NCount+=1.0
-    except:
-        continue
+#Dataset to Dictionary Mapping
+sentenceDictionary,SCount,NCount = datasetDictCreation(filename)
 
+#Model Creation (If not Created)
 '''
-for k in sentenceDictionary:
-    if k['category']=="0":
-        notSarcasmDict = dictAddition(notSarcasmDict,k['text'])
-    else:
-        SarcasmDict = dictAddition(SarcasmDict,k['text'])
-
-with open("sarcasmDict.pkl", "wb") as f:
-    pickle.dump(SarcasmDict, f)
-
-with open("NotSarcasmDict.pkl", "wb") as f:
-    pickle.dump(notSarcasmDict, f)
+SarcasmDict,notSarcasmDict = separateDictionaryCreation(notSarcasmDict,SarcasmDict,sentenceDictionary)
+pickleUnload("sarcasmDict.pkl",SarcasmDict)
+pickleUnload("NotSarcasmDict.pkl",notSarcasmDict)
 '''
 
-with open("sarcasmDict.pkl", "rb") as f:
-    SarcasmDict = pickle.load(f)
+#Downloading Models
+SarcasmDict = pickleLoad("sarcasmDict.pkl")
+notSarcasmDict = pickleLoad("NotSarcasmDict.pkl")
 DictionaryAll = SarcasmDict
-
-with open("NotSarcasmDict.pkl", "rb") as f:
-    notSarcasmDict = pickle.load(f)
 DictionaryAll.update(notSarcasmDict)
 
+#Sentence Classification
 testSentence = "if i had my license the only thing i'd use it for is mcdonalds at 3am #sarcasm"
-tokens = nltk.word_tokenize(testSentence.lower())
-tokenFiltered = []
-for token in tokens:
-    if token not in stop_words:
-        tokenFiltered.append(token)
+bayesProbability_1,bayesProbability_0 = bayesianClassification(testSentence,SarcasmDict,notSarcasmDict,SCount,NCount)
 
-for token in tokenFiltered:
-    try:
-        bayesProbability_1 *= (1.0+SarcasmDict[token])/(len(SarcasmDict)+len(DictionaryAll))
-        bayesProbability_0 *= (1.0+notSarcasmDict[token])/(len(notSarcasmDict)+len(DictionaryAll))
-        
-    except:
-        bayesProbability_1 *= (1.0)/(len(SarcasmDict)+len(DictionaryAll))
-        bayesProbability_0 *= (1.0)/(len(notSarcasmDict)+len(DictionaryAll))
-   
-
-    
-
-bayesProbability_1 *= (SCount/SCount+NCount)
-bayesProbability_0 *= (NCount/SCount+NCount)
-
+#Output
+print "\n"
 print "Sarcastic Sentiment Analysis".center(50,"-")
+print "\n"
 print "Sentence: "+testSentence
-
 if(bayesProbability_1>bayesProbability_0):
     print "Result: Sarcastic"
 else:
     print "Result: Not Sarcastic"
 print "\n"
-
-print "Sarcasm Probability: "+ str(bayesProbability_1)
-print "Non Sarcasm Probability: "+ str(bayesProbability_0)
+print "Sarcastic Probability: "+ str(bayesProbability_1)
+print "Non Sarcastic Probability: "+ str(bayesProbability_0)
 print "\n"
